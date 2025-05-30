37e0ab3a6e6b1dca84bb3176c1e58e30101e53df
zoli0829 <zoli0829@gmail.com>
2025-05-30 11:56:03 +0100

    Initial commit

---
+++ LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Zoltan Vegh on 30/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
