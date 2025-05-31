9e17fbe8b32364b27c15b515b48942bc40094f08
zoli0829 <zoli0829@gmail.com>
2025-05-31 09:36:27 +0100

    finished day 93

--- LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/ContentView.swift
@@ -8,10 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    
     var body: some View {
-        Text("Hello World!")
-            .background(.red)
-            .offset(x: 100, y: 100)
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 0) {
+                ForEach(1..<20) { number in
+                        Text("Number \(number)")
+                            .font(.largeTitle)
+                            .padding()
+                            .background(.red)
+                            .frame(width: 200, height: 200)
+                            .visualEffect({ content, proxy in
+                                content
+                                    .rotation3DEffect(
+                                        .degrees(-proxy.frame(in: .global).minX / 8),
+                                        axis: (x: 0, y: 1, z: 0)
+                                    )
+                            })
+                }
+            }
+            .scrollTargetLayout()
+        }
+        .scrollTargetBehavior(.viewAligned)
     }
 }
 
